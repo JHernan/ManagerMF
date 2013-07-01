5536be9d64e030a84292e2be451081039fcd0341
jorge <jorge.hernan@radmas.com>
2013-07-01 21:08:23 +0200

    Integración con Capifony

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,19 +1,28 @@
-set :application, "set your application name here"
-set :domain,      "#{application}.com"
-set :deploy_to,   "/var/www/#{domain}"
+set :application, "manager"
+set :domain,      "mushofutbol.com"
+set :deploy_to,   "/home/www-data/#{application}"
 set :app_path,    "app"
+set :web_path,    "web"
 
-set :repository,  "#{domain}:/var/repos/#{application}.git"
+set :repository,  "https://github.com/JHernan/ManagerMF.git"
 set :scm,         :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
 
 set :model_manager, "doctrine"
 # Or: `propel`
 
+set :user, "root"
+
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain, :primary => true       # This may be the same as your `Web` server
 
+set  :use_sudo,      false
 set  :keep_releases,  3
 
+set :shared_files,      ["app/config/parameters.yml"]
+set :shared_children,   [app_path + "/logs", web_path + "/uploads", "vendor"]
+
+set :use_composer, true
+
 # Be more verbose by uncommenting the following line
-# logger.level = Logger::MAX_LEVEL
+logger.level = Logger::MAX_LEVEL
