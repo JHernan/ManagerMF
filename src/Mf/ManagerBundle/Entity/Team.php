4b88a9f84d80cb52cead885da849f62568d0eb08
jorge <jorge.hernan@radmas.com>
2013-07-14 20:42:42 +0200

    Añadidas relaciones a Team

--- src/Mf/ManagerBundle/Entity/Team.php
+++ src/Mf/ManagerBundle/Entity/Team.php
@@ -28,6 +28,24 @@ class Team
      */
     private $name;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id", nullable=false)
+     */
+    private $user_id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="League")
+     * @ORM\JoinColumn(name="league_id", referencedColumnName="id", nullable=false)
+     */
+    private $league;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Tactic")
+     * @ORM\JoinColumn(name="tactic_id", referencedColumnName="id", nullable=false)
+     */
+    private $tactic;
+
 
     /**
      * Get id
@@ -60,5 +78,74 @@ class Team
     public function getName()
     {
         return $this->name;
+    }
+
+    /**
+     * Set user_id
+     *
+     * @param \Mf\ManagerBundle\Entity\User $userId
+     * @return Team
+     */
+    public function setUserId(\Mf\ManagerBundle\Entity\User $userId)
+    {
+        $this->user_id = $userId;
+    
+        return $this;
+    }
+
+    /**
+     * Get user_id
+     *
+     * @return \Mf\ManagerBundle\Entity\User 
+     */
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * Set league
+     *
+     * @param \Mf\ManagerBundle\Entity\League $league
+     * @return Team
+     */
+    public function setLeague(\Mf\ManagerBundle\Entity\League $league)
+    {
+        $this->league = $league;
+    
+        return $this;
+    }
+
+    /**
+     * Get league
+     *
+     * @return \Mf\ManagerBundle\Entity\League 
+     */
+    public function getLeague()
+    {
+        return $this->league;
+    }
+
+    /**
+     * Set tactic
+     *
+     * @param \Mf\ManagerBundle\Entity\Tactic $tactic
+     * @return Team
+     */
+    public function setTactic(\Mf\ManagerBundle\Entity\Tactic $tactic)
+    {
+        $this->tactic = $tactic;
+    
+        return $this;
+    }
+
+    /**
+     * Get tactic
+     *
+     * @return \Mf\ManagerBundle\Entity\Tactic 
+     */
+    public function getTactic()
+    {
+        return $this->tactic;
     }
 }
