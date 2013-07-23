349268874770dc59c4d096fbe1e32ad69b365628
jorge <jorge.hernan@radmas.com>
2013-07-23 13:20:23 +0200

    Generado módulo de Administración de User

---
+++ src/Mf/UserBundle/Admin/Entity/UserAdmin.php
@@ -0,0 +1,83 @@
+<?php
+namespace Mf\UserBundle\Admin\Entity;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+use FOS\UserBundle\Model\UserManagerInterface;
+
+class UserAdmin extends Admin
+{
+
+    protected $userManager;
+    protected $baseRouteName = 'user_admin';
+
+    protected function configureRoutes(RouteCollection $collection)
+    {        
+        $collection->add('view', $this->getRouterIdParameter().'/view');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('username')
+            ->add('email')
+            ->add('enabled')
+        ;
+    }
+
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('General')
+                ->add('username')
+                ->add('email')
+                ->add('plainPassword', 'text')
+            ->end()
+            /*
+            ->with('Groups')
+                ->add('groups', 'sonata_type_model', array('required' => false))
+            ->end()
+            */
+            ->with('Management')
+                /*
+                ->add('roles', 'sonata_security_roles', array( 'multiple' => true))
+                */
+                ->add('locked', null, array('required' => false))
+                ->add('expired', null, array('required' => false))
+                ->add('enabled', null, array('required' => false))
+                ->add('credentialsExpired', null, array('required' => false))
+            ->end()
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $filter)
+    {
+        $filter
+            ->add('username')
+            ->add('email')
+            ->add('enabled')
+        ;
+    }
+
+    public function preUpdate($user)
+    {
+        $this->getUserManager()->updateCanonicalFields($user);
+        $this->getUserManager()->updatePassword($user);
+    }
+
+    public function setUserManager(UserManagerInterface $userManager)
+    {
+        $this->userManager = $userManager;
+    }
+
+    /**
+     * @return UserManagerInterface
+     */
+    public function getUserManager()
+    {
+        return $this->userManager;
+    }
+}
