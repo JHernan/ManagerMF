76f5aa9e1ff235db401132d1b8d56c1e1f13f53e
jorge <jorge.hernan@radmas.com>
2013-07-24 13:08:32 +0200

    Generado módulo de Administración de Player

---
+++ src/Mf/ManagerBundle/Admin/Entity/PlayerAdmin.php
@@ -0,0 +1,43 @@
+<?php
+namespace Mf\ManagerBundle\Admin\Entity;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+
+class PlayerAdmin extends Admin
+{
+
+    protected $baseRouteName = 'player_admin';
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('name')
+            ->add('football_team')
+        ;
+    }
+
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('General')
+            ->add('name')
+            ->add('football_team')
+            ->add('demarcations', null,
+                array('required' => false, 'expanded' => true))
+            ->add('team_players', null,
+                array('required' => false, 'expanded' => true))
+            ->end()
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $filter)
+    {
+        $filter
+            ->add('name')
+            ->add('football_team')
+        ;
+    }
+}
