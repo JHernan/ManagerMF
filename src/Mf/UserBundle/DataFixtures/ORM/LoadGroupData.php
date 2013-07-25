c7bafd2a51c26a5132e36fbe79cfdc3a857fecf6
jorge <jorge.hernan@radmas.com>
2013-07-25 22:07:46 +0200

    Configurados ROLE_USER y ROLE_SUPER_ADMIN y actualizados Fixtures

---
+++ src/Mf/UserBundle/DataFixtures/ORM/LoadGroupData.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Mf\UserBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Mf\UserBundle\Entity\Group;
+
+class LoadGroupData extends AbstractFixture implements OrderedFixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $groupUser = new Group('User');
+        $groupUser->addRole('ROLE_USER');
+        $this->addReference('group-user', $groupUser);       
+        $manager->persist($groupUser);
+
+        $groupAdmin = new Group('Admin');
+        $groupAdmin->addRole('ROLE_SUPER_ADMIN');
+        $this->addReference('group-admin', $groupAdmin);       
+        $manager->persist($groupAdmin);
+
+        $manager->flush();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 13; // the order in which fixtures will be loaded
+    }
+}
+
+?>
--- src/Mf/UserBundle/DataFixtures/ORM/LoadUserData.php
+++ src/Mf/UserBundle/DataFixtures/ORM/LoadUserData.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Mf\ManagerBundle\DataFixtures\ORM;
+namespace Mf\UserBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
@@ -18,39 +18,52 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
         $raw = '123456';
         $users = array(
                         array(
+                            'name' => 'Admin',
+                            'email' => 'admin@test.com',
+                            'salt' => $salt,
+                            'password' => md5(md5($raw) . $salt),
+                            'enabled' => 1,
+                            'group' => 'group-admin'
+                            ),
+                        array(
                             'name' => 'ACastilla',
                             'email' => 'test+1@test.com',
                             'salt' => $salt,
                             'password' => md5(md5($raw) . $salt),
-                            'enabled' => 1
+                            'enabled' => 1,
+                            'group' => 'group-user'
                             ),
                         array(
                             'name' => 'Rai',
                             'email' => 'test+2@test.com',
                             'salt' => $salt,
                             'password' => md5(md5($raw) . $salt),
-                            'enabled' => 1
+                            'enabled' => 1,
+                            'group' => 'group-user'
                             ),
                         array(
                             'name' => 'Arvin',
                             'email' => 'test+3@test.com',
                             'salt' => $salt,
                             'password' => md5(md5($raw) . $salt),
-                            'enabled' => 1
+                            'enabled' => 1,
+                            'group' => 'group-user'
                             ),
                         array(
                             'name' => '7Sins',
                             'email' => 'test+4@test.com',
                             'salt' => $salt,
                             'password' => md5(md5($raw) . $salt),
-                            'enabled' => 1
+                            'enabled' => 1,
+                            'group' => 'group-user'
                             ),
                         array(
                             'name' => 'Deco',
                             'email' => 'test+5@test.com',
                             'salt' => $salt,
                             'password' => md5(md5($raw) . $salt),
-                            'enabled' => 1
+                            'enabled' => 1,
+                            'group' => 'group-user'
                             ),
                         );
         foreach($users as $item):
@@ -60,6 +73,7 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
             $user->setSalt($item['salt']);
             $user->setPassword($item['password']);
             $user->setEnabled($item['enabled']);
+            $user->addGroup($this->getReference($item['group']));
 
             $manager->persist($user);
 
@@ -74,7 +88,7 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
      */
     public function getOrder()
     {
-        return 12; // the order in which fixtures will be loaded
+        return 14; // the order in which fixtures will be loaded
     }
 }
 
