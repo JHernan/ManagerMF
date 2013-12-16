57e00716cf1258cb31a7726543fa8e46c18a52bc
Jorge Hernán <jorge.hernan.esparza@gmail.com>
2013-12-16 22:40:30 +0100

    Actualizadas configuraciones de BBDD y Capifony

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -6,15 +6,15 @@ set :web_path,    "web"
 
 set :repository,  "https://github.com/JHernan/ManagerMF.git"
 set :scm,         :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
 
 set :model_manager, "doctrine"
-# Or: `propel`
 
 set :user, "root"
+server domain, :app, :web, :primary => true
 
 role :web,        domain                         # Your HTTP server, Apache/etc
-role :app,        domain, :primary => true       # This may be the same as your `Web` server
+role :app,        domain                         # This may be the same as your `Web` server
+role :db,         domain, :primary => true       # This is wdhere Symfony2 migrations will run
 
 set  :use_sudo,      false
 set  :keep_releases,  3
@@ -24,7 +24,10 @@ set :shared_children,   [app_path + "/logs", web_path + "/uploads", "vendor"]
 
 set :use_composer, true
 
+set :deploy_via, :remote_cache
+
 set :dump_assetic_assets, true
+set :normalize_asset_timestamps, false
 
 # Be more verbose by uncommenting the following line
 logger.level = Logger::MAX_LEVEL
